9df595b643bae17d9d103e9e7755e1a1a92f2e7d
oof <oonxt@msn.com>
2026-03-02 17:44:21 +0800

    feat: add core index exports

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { EditorServer } from './core/editor-server'
+export { X2tConverter } from './core/x2t-converter'
+export { MockSocket, io } from './core/mock-socket'
+export { createXHRProxy } from './core/xhr-proxy'
+export { getDocumentType, getFileExt } from './core/utils'
+export type {
+  DocEditor,
+  User,
+  Participant,
+  X2tConvertParams,
+  X2tConvertResult,
+  OfficeTheme,
+} from './core/types'
